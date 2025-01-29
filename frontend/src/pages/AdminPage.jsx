16e0e2f1b15dde14d2d04994c21b81b9e2a64049
Himanshu <himanshubindal90340@gmail.com>
2025-01-29 22:01:35 +0530

    error solved

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,8 +1,9 @@
 import { BarChart, PlusCircle, ShoppingBasket } from "lucide-react";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import CreateProductForm from "../components/CreateProductForm";
+
 import AnalyticsTab from "../components/AnalyticsTab";
+import CreateProductForm from "../components/CreateProductForm";
 import ProductsList from "../components/ProductsList";
 import { useProductStore } from "../stores/useProductStore";
 
@@ -14,17 +15,17 @@ const tabs = [
 
 const AdminPage = () => {
 	const [activeTab, setActiveTab] = useState("create");
-	const {fetchAllProducts}=useProductStore();
+	const { fetchAllProducts } = useProductStore();
 
-	useEffect(()=>{
+	useEffect(() => {
 		fetchAllProducts();
-	},[fetchAllProducts]);
+	}, [fetchAllProducts]);
 
 	return (
-		<div className="min-h-screen relative overflow-hidden">
-			<div className="relative z-10 container mx-auto px-4 py-16">
+		<div className='min-h-screen relative overflow-hidden'>
+			<div className='relative z-10 container mx-auto px-4 py-16'>
 				<motion.h1
-					className="text-4xl font-bold mb-8 text-emerald-400 text-center"
+					className='text-4xl font-bold mb-8 text-emerald-400 text-center'
 					initial={{ opacity: 0, y: -20 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.8 }}
@@ -32,7 +33,7 @@ const AdminPage = () => {
 					Admin Dashboard
 				</motion.h1>
 
-				<div className="flex justify-center mb-8">
+				<div className='flex justify-center mb-8'>
 					{tabs.map((tab) => (
 						<button
 							key={tab.id}
@@ -43,12 +44,11 @@ const AdminPage = () => {
 									: "bg-gray-700 text-gray-300 hover:bg-gray-600"
 							}`}
 						>
-							<tab.icon className="mr-2 h-5 w-5" />
+							<tab.icon className='mr-2 h-5 w-5' />
 							{tab.label}
 						</button>
 					))}
 				</div>
-
 				{activeTab === "create" && <CreateProductForm />}
 				{activeTab === "products" && <ProductsList />}
 				{activeTab === "analytics" && <AnalyticsTab />}
@@ -56,5 +56,4 @@ const AdminPage = () => {
 		</div>
 	);
 };
-
 export default AdminPage;
--- frontend/src/pages/PurchaseCancelPage.jsx
+++ frontend/src/pages/PurchaseCancelPage.jsx
@@ -1,3 +1,4 @@
+
 import { XCircle, ArrowLeft } from "lucide-react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
